7617a1ca68225217368b51bb53fc18992dbe0b81
syco4859-source <syco4859@gmail.com>
2026-02-21 12:00:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
 import os
-TOKEN = os.getenv("8542846687:AAGxY_wYFouSN5rHHyuK4ZZdX4s3t4b8KHg")"
+TOKEN = os.getenv("8542846687:AAGxY_wYFouSN5rHHyuK4ZZdX4s3t4b8KHg"
 BASE_DIR = "projects"
 os.makedirs(BASE_DIR, exist_ok=True)
 
